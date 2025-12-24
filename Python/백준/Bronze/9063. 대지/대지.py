7db62c09945b0f90f55526060a0fdea0cb5df371
Eunbee Kim <yokohamay0612@gmail.com>
2025-12-24 23:24:39 +0900

    [Bronze III] Title: 대지, Time: 76 ms, Memory: 36352 KB -BaekjoonHub

--- Python/백준/Bronze/9063. 대지/대지.py
+++ Python/백준/Bronze/9063. 대지/대지.py
@@ -1,15 +1,14 @@
 import sys
+input = sys.stdin.readline
+n= int(input())
+x_list = []
+y_list = []
+for i in range(n):
+    x,y =map(int,input().split())
+    x_list.append(x)
+    y_list.append(y)
 
-n = int(sys.stdin.readline())
+x_result = max(x_list)-min(x_list)
+y_result = max(y_list)-min(y_list)
 
-min_x, max_x = 10001, -10001
-min_y, max_y = 10001, -10001
-
-for _ in range(n):
-    x, y = map(int, sys.stdin.readline().split())
-    if x < min_x: min_x = x
-    if x > max_x: max_x = x
-    if y < min_y: min_y = y
-    if y > max_y: max_y = y
-
-print((max_x - min_x) * (max_y - min_y))
+print(x_result * y_result)
