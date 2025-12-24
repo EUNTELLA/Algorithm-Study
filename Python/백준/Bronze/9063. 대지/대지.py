e9435b331381367a1ad55c7fbaf821a9396b550b
Eunbee Kim <yokohamay0612@gmail.com>
2025-12-24 23:21:43 +0900

    [Bronze III] Title: 대지, Time: 1312 ms, Memory: 35916 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,12 @@
+n= int(input())
+x_list = []
+y_list = []
+for i in range(n):
+    x,y =map(int,input().split())
+    x_list.append(x)
+    y_list.append(y)
+
+x_result = max(x_list)-min(x_list)
+y_result = max(y_list)-min(y_list)
+
+print(x_result * y_result)
